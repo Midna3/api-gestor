cec3d6f73f76c4c99924846c9a5da5e539de6ede
Arthur Scanoni <arthur.scanoni@hotmail.com>
2022-04-13 13:31:21 -0300

    feat: Start the Spring Initializr
    start a project in spring

---
+++ src/main/java/edu/apigestor/ApiGestorApplication.java
@@ -0,0 +1,13 @@
+package edu.apigestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/apigestor/ApiGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.apigestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
