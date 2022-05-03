39f7c9412e6e6e12df84dad676bf43dbe0e86f72
Moésio Filho <moesio.f@gmail.com>
2022-05-03 13:15:59 -0300

    feat: add entity HomeDadosNacionalResponse

---
+++ src/main/java/edu/apigestor/entity/response/HomeDadosNacionalResponse.java
@@ -0,0 +1,31 @@
+package edu.apigestor.entity.response;
+
+import java.util.Map;
+
+/**
+ * <p>Essa classe representa uma resposta para solicitações de dados gerais sobre a educação
+ * brasileira a nível nacional.</p>
+ *
+ * @author moesiof
+ * @version 1.0
+ * @see HomeDadosResponse
+ */
+public class HomeDadosNacionalResponse extends HomeDadosResponse {
+
+  private String country = "Brasil"; // País padrão da resposta.
+
+  /**
+   * Adiciona o país dessa resposta.
+   *
+   * @return essa mesma resposta, permitindo <i>method chaining</i>.
+   */
+  public HomeDadosNacionalResponse country(String country) {
+    this.country = country;
+    return this;
+  }
+
+  @Override
+  protected Map<String, Object> additionalAttributes() {
+    return Map.of("country", this.country);
+  }
+}
