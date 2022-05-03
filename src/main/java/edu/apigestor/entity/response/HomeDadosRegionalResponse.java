c09f7ecb38e20c12d9c10fe49e2bb2a20bb3ab4b
Moésio Filho <moesio.f@gmail.com>
2022-05-03 13:12:39 -0300

    feat: add entity HomeDadosRegionalResponse

---
+++ src/main/java/edu/apigestor/entity/response/HomeDadosRegionalResponse.java
@@ -0,0 +1,31 @@
+package edu.apigestor.entity.response;
+
+import java.util.Map;
+
+/**
+ * <p>Essa classe representa uma resposta para solicitações de dados gerais sobre a educação
+ * brasileira a nível regional.</p>
+ *
+ * @author moesiof
+ * @version 1.0
+ * @see HomeDadosResponse
+ */
+public class HomeDadosRegionalResponse extends HomeDadosResponse {
+
+  private String region;
+
+  /**
+   * Adiciona a região dessa resposta.
+   *
+   * @return essa mesma resposta, permitindo <i>method chaining</i>.
+   */
+  public HomeDadosRegionalResponse region(String region) {
+    this.region = region;
+    return this;
+  }
+
+  @Override
+  protected Map<String, Object> additionalAttributes() {
+    return Map.of("region", this.region);
+  }
+}
