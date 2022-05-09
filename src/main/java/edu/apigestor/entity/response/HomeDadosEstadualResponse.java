7e21c4dfb04fb89d223eb80ec537c0689f898a46
lucas-lins <lucaslins.link@gmail.com>
2022-05-09 19:48:27 -0300

    feat: entity add feat entity home dados estadual response

---
+++ src/main/java/edu/apigestor/entity/response/HomeDadosEstadualResponse.java
@@ -0,0 +1,31 @@
+package edu.apigestor.entity.response;
+
+import java.util.Map;
+
+/**
+ * <p>Essa classe representa uma resposta para solicitações de dados gerais sobre a educação
+ * brasileira a nível nacional.</p>
+ *
+ * @author lucas-lins
+ * @version 1.0
+ * @see HomeDadosResponse
+ */
+public class HomeDadosEstadualResponse extends HomeDadosResponse{
+
+    private String state;
+  /**
+   * Adiciona o país dessa resposta.
+   *
+   * @param State nome do país.
+   * @return essa mesma resposta, permitindo <i>method chaining</i>.
+   */
+  public HomeDadosEstadualResponse state(String state) {
+    this.state = state;
+    return this;
+  }
+
+  @Override
+  protected Map<String, Object> additionalAttributes() {
+    return Map.of("state", this.state);
+  }    
+}
