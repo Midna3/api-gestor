73ccb523866c3a55a40412d534f0c73880d4ea3c
Moésio Filho <moesio.f@gmail.com>
2022-05-09 09:53:51 -0300

    fix: fix implementing interface of ComparacaoIdeb

--- src/main/java/edu/apigestor/control/services/imp/ComparacaoIdeb.java
+++ src/main/java/edu/apigestor/control/services/imp/ComparacaoIdeb.java
@@ -1,12 +1,12 @@
 package edu.apigestor.control.services.imp;
 
-import edu.apigestor.control.services.IPainelEscolaService;
+import edu.apigestor.control.services.IComparacaoIdebService;
 import edu.apigestor.data.repository.IDEBRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ComparacaoIdeb implements IPainelEscolaService {
+public class ComparacaoIdeb implements IComparacaoIdebService {
 
   @Autowired
   private IDEBRepository idebRepository;
