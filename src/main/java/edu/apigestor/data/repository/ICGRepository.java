7a8f9d51bd78404d20a1cb50e3bc74f4312b46d4
Moésio Filho <moesio.f@gmail.com>
2022-05-09 09:38:15 -0300

    refactor: extends default Repository

--- src/main/java/edu/apigestor/data/repository/ICGRepository.java
+++ src/main/java/edu/apigestor/data/repository/ICGRepository.java
@@ -1,5 +1,7 @@
 package edu.apigestor.data.repository;
 
+import org.springframework.data.repository.Repository;
+
 /**
  * Essa interface possui métodos que operam na coleção de todas as entidades ICG presentes no Banco
  * de Dados. Os diferentes métodos desse repositório são utilizados pelo pacote de serviços da
@@ -7,6 +9,6 @@ package edu.apigestor.data.repository;
  *
  * @version 0.1
  */
-public interface ICGRepository {
+public interface ICGRepository extends Repository<Object, Object> {
 
 }
--- src/main/java/edu/apigestor/data/repository/IDEBRepository.java
+++ src/main/java/edu/apigestor/data/repository/IDEBRepository.java
@@ -1,5 +1,7 @@
 package edu.apigestor.data.repository;
 
+import org.springframework.data.repository.Repository;
+
 /**
  * Essa interface possui métodos que operam na coleção de todas as entidades IDEB presentes no Banco
  * de Dados. Os diferentes métodos desse repositório são utilizados pelo pacote de serviços da
@@ -7,6 +9,6 @@ package edu.apigestor.data.repository;
  *
  * @version 0.1
  */
-public interface IDEBRepository {
+public interface IDEBRepository extends Repository<Object, Object> {
 
 }
