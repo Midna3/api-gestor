8099777c77e97a424f0bf14c225faab6859fc78c
Moésio Filho <moesio.f@gmail.com>
2022-05-11 09:52:35 -0300

    feat: add IED to Home.dataCountry(...)

--- src/main/java/edu/apigestor/control/services/imp/Home.java
+++ src/main/java/edu/apigestor/control/services/imp/Home.java
@@ -1,19 +1,28 @@
 package edu.apigestor.control.services.imp;
 
+import edu.apigestor.control.mappers.CategoryMapper;
+import edu.apigestor.control.mappers.CountryMapper;
+import edu.apigestor.control.mappers.CountryMapper.CountryID;
 import edu.apigestor.control.services.IHomeService;
+import edu.apigestor.control.utils.AvailableYears;
 import edu.apigestor.data.repository.AFDRepository;
 import edu.apigestor.data.repository.ICGRepository;
 import edu.apigestor.data.repository.IDEBRepository;
 import edu.apigestor.data.repository.IEDRepository;
 import edu.apigestor.data.repository.IRDRepository;
 import edu.apigestor.data.repository.TDIRepository;
+import edu.apigestor.entity.domain.IED;
 import edu.apigestor.entity.response.HomeDadosNacionalResponse;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Home implements IHomeService {
+
+  private final AtomicLong count = new AtomicLong();
 
   @Autowired
   private IEDRepository iedRepository;
@@ -35,6 +44,39 @@ public class Home implements IHomeService {
 
   @Override
   public ResponseEntity<HomeDadosNacionalResponse> dataCountry(String country, int year) {
-    return null;
+    long responseID = this.count.getAndIncrement();
+    HomeDadosNacionalResponse response = new HomeDadosNacionalResponse();
+
+    if (!AvailableYears.isAvailable(year)) {
+      response.addError(responseID,
+          HttpStatus.BAD_REQUEST.value(),
+          "Year '%d' is unavailable.".formatted(year));
+      return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    CountryID countryID = CountryMapper.getCountry(country);
+    int code = countryID.code();
+    String countryName = countryID.name();
+
+    if (code == -1) {
+      response.addError(responseID,
+          HttpStatus.BAD_REQUEST.value(),
+          "Country '%s' doesn't exist.".formatted(country));
+      return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    IED ied = this.iedRepository.getIEDForCountry(code, year);
+    double meanIED = (1 * ied.getPercentageN1() +
+        2 * ied.getPercentageN2() +
+        3 * ied.getPercentageN3() +
+        4 * ied.getPercentageN4() +
+        5 * ied.getPercentageN5() +
+        6 * ied.getPercentageN6()) / 100;
+
+    response.country(countryName)
+        .id(responseID)
+        .ied(meanIED, CategoryMapper.getIEDCategory(meanIED));
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 }
