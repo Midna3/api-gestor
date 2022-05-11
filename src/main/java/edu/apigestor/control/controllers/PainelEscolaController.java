a5351418cd705c09c5c2447ca262efa02ad12f66
Arthur Scanoni <arthur.scanoni@hotmail.com>
2022-05-11 17:25:49 -0300

    fix: add permission to make request

--- src/main/java/edu/apigestor/control/controllers/PainelEscolaController.java
+++ src/main/java/edu/apigestor/control/controllers/PainelEscolaController.java
@@ -5,11 +5,7 @@ import edu.apigestor.control.services.IPainelEscolaService;
 import edu.apigestor.entity.response.PainelEscolaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controlador do painel da escola.
@@ -18,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "/panel")
+@CrossOrigin
 public class PainelEscolaController {
 
   @Autowired
