9025c902f174cd98c24cf1860a15f82c3d53b9e5
Moésio Filho <moesio.f@gmail.com>
2022-05-14 19:06:56 -0300

    feat: add new method to AvailableYears
    - Add isIdebAvailable(int);

--- src/main/java/edu/apigestor/control/utils/AvailableYears.java
+++ src/main/java/edu/apigestor/control/utils/AvailableYears.java
@@ -7,11 +7,17 @@ public final class AvailableYears {
 
   private static final int[] YEARS = IntStream.range(2017, 2020 + 1).toArray();
 
+  private static final int[] YEARS_IDEB = new int[]{2017, 2019};
+
   private AvailableYears() {
 
   }
 
   public static boolean isAvailable(int year) {
     return Arrays.stream(AvailableYears.YEARS).anyMatch(i -> i == year);
+  }
+
+  public static boolean isIdebAvailable(int year) {
+    return Arrays.stream(AvailableYears.YEARS_IDEB).anyMatch(i -> i == year);
   }
 }
